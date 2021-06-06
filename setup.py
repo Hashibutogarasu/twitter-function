cfd4ae42dcf75ce50fa3a586b0100caca0824a9d
カラス <71928247+Hashibutogarasu@users.noreply.github.com>
2021-06-06 19:49:45 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+ 
+setuptools.setup(
+    name="twitter_function",
+    version="1.0",
+    author="Columba_Karasu",
+    author_email="garasuhashiboso@gmail.com",
+    description="tweepy functions",
+    long_description="This library makes it easy to use tweepy.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Hashibutogarasu/twitter-function",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9.4",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
